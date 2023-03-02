27fff83e11c45b7ddcd6eb258ac6d48c0b223d57
Zach <dzacharymason@gmail.com>
2023-03-02 01:19:07 -0600

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,9 +18,12 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Look at us doing things!&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
+          <h1 className="title">
+            Welcome to <a href="/posts/first-post">this</a>
+          </h1>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
